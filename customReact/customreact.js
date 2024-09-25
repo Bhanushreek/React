7924ed107510708a75a2994c6a749753b3ec0ebe
Bhanushreek <kundurbhanushreereddy@gmail.com>
2024-09-25 20:02:41 +0530

    custom react

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,3 +1,24 @@
+function customRender (reactElement, container) {
+    /*
+    const domELement = document.createElement
+    (reactElement.type)
+    domELement.innerHTML = reactElement.children
+    domELement.setAttribute('href', reactElement.props.href)
+    domELement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domELement)
+    */
+
+    const domELement = document.createElement
+    (reactElement.type)
+    domELement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domELement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domELement)
+}
+
 const reactElement = {
     type: 'a',
     props: {
@@ -9,3 +30,4 @@ const reactElement = {
 
 const mainContainer = document.querySelector("#root")
 
+customRender(reactElement, mainContainer)
